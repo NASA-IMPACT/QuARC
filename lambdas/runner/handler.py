9ec52b168f47964025ab344079d7168aecf0fe22
“slesaad” <slesaad@gmail.com>
2022-05-10 14:29:06 -0500

    Update lambda input expectation and output

--- lambdas/runner/handler.py
+++ lambdas/runner/handler.py
@@ -1,19 +1,36 @@
 from pyQuARC import ARC
+import json
 
 
 def handler(event, context):
-    arc = ARC(
-        input_concept_ids=[event.get('concept_id')],
-        metadata_format=event.get('format', 'echo10'),
-    )
-    results = arc.validate()
-    return results
+    request_body = json.loads(event.get("body", "{}"))
+
+    response = {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        "body": ""
+    }
+    try:
+        arc = ARC(
+            input_concept_ids=[request_body.get('concept_id')],
+            metadata_format=request_body.get('format', 'echo10'),
+        )
+        results = arc.validate()
+        response['body'] = json.dumps(results)
+    except Exception as e:
+        response['statusCode'] = 500
+        response['body'] = str(e)
+    return response
+        
 
 
 if __name__=='__main__':
     sample_event = {
-        "concept_id": "C1214470488-ASF",
-        "format": "echo10",
+        "body": json.dumps({
+            "concept_id": "C1214470488-ASF",
+            "format": "echo10"
+        })
     }
 
     print(handler(sample_event, None))
