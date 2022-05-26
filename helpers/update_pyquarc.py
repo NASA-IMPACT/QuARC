4700599f0ec99f81815dc0e124ce26b37c1a0f7b
“slesaad” <slesaad@gmail.com>
2022-05-26 11:47:11 -0500

    Add comments

--- helpers/update_pyquarc.py
+++ helpers/update_pyquarc.py
@@ -2,28 +2,43 @@ import requests, zipfile, io, os
 
 from utils import get_parameter, ssm_param_name
 
-owner = "NASA-IMPACT"
-repo = "pyQuARC"
+OWNER = "NASA-IMPACT"
+REPO = "pyQuARC"
 
-api_url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
-response = requests.get(api_url).json()
-version = response.get("tag_name", "v1.0.0")[1:]
+API_URL = f"https://api.github.com/repos/{OWNER}/{REPO}/releases/latest"
 
-current_pyquarc_version = get_parameter(ssm_param_name)
+response = requests.get(API_URL)
+response.raise_for_status()
 
-if version != current_pyquarc_version:
+response = response.json()
+version = response.get("tag_name", "v1.0.0")
+
+# Get the current version being used by QuARC
+curr_version = get_parameter(ssm_param_name)
+
+if version != curr_version:
+    # Zip of the latest release
     zip_url = response["zipball_url"]
+
     zip_response = requests.get(zip_url)
+    zip_response.raise_for_status()
     zip_file = zipfile.ZipFile(io.BytesIO(zip_response.content))
+
+    # Extract the zip file to the tmp/ directory
     zip_file.extractall("tmp/")
+
+    # Get the first directory in the list (folder name is pyQuARC<SOMEHASH>)
     directory = [x for x in os.listdir('tmp/') if os.path.isdir(f"tmp/{x}")][0]
+
+    # Run pyQuARC installation
     return_code = os.system(f"pip install tmp/{directory} --target=layers/pyQuARC/python")
 
     if return_code == 0:
+        # If the installation succeeds, set required env variables to be used in next steps
         env_file = os.environ.get('GITHUB_ENV')
 
         with open(env_file, "a") as myfile:
             myfile.write("NEEDS_DEPLOYMENT=true\n")
             myfile.write(f"NEW_VERSION={version}")
     else:
-        print("Failed to update pyQuARC")
+        raise Exception("Failed to update pyQuARC")
