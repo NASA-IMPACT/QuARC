52355c73f4f50874c0ebf951069215146594432c
Rajesh Pandey <rajesh@Rajeshs-MacBook-Pro.local>
2022-06-21 09:43:46 -0500

    change to RestAPI and add model templates for requests

--- deploy/deploy/stack.py
+++ deploy/deploy/stack.py
@@ -40,19 +40,108 @@ class AppStack(core.Stack):
         )
 
         # The API Gateway that the integrates with the lambda function, where clients can submit requests
-        api = apigateway.LambdaRestApi(
+        api = apigateway.RestApi(
             self,
             f"{construct_id}-api",
-            handler=runner,
-            default_cors_preflight_options=apigateway.CorsOptions(
-                allow_origins=apigateway.Cors.ALL_ORIGINS, allow_methods=apigateway.Cors.ALL_METHODS
-            ),
             deploy_options=apigateway.StageOptions(
                 stage_name=config.ENV,
+            ),
+            default_cors_preflight_options=apigateway.CorsOptions(
+                allow_origins=apigateway.Cors.ALL_ORIGINS, allow_methods=apigateway.Cors.ALL_METHODS
             ),
             rest_api_name=f"{construct_id}-api",
             binary_media_types=["*/*"],
         )
+        # api = apigateway.LambdaRestApi(
+        #     self,
+        #     f"{construct_id}-api",
+        #     handler=runner,
+        #     proxy=False,
+        #     default_cors_preflight_options=apigateway.CorsOptions(
+        #         allow_origins=apigateway.Cors.ALL_ORIGINS, allow_methods=apigateway.Cors.ALL_METHODS
+        #     ),
+        #     deploy_options=apigateway.StageOptions(
+        #         stage_name=config.ENV,
+        #     ),
+        #     rest_api_name=f"{construct_id}-api",
+        #     binary_media_types=["*/*"],
+        # )
 
         validate = api.root.add_resource("validate")
-        validate.add_method("POST")
+        request_model = api.add_model(
+            "RequestModel",
+            content_type="application/json",
+            model_name="RequestModel",
+            schema=apigateway.JsonSchema(
+                schema=apigateway.JsonSchemaVersion.DRAFT4,
+                title="pollRequest",
+                type=apigateway.JsonSchemaType.OBJECT,
+                properties={
+                    "format": apigateway.JsonSchema(type=apigateway.JsonSchemaType.STRING),
+                    "concept_id": apigateway.JsonSchema(type=apigateway.JsonSchemaType.STRING)
+                    # "file": apigateway.JsonSchema(type=apigateway.JsonSchemaType.OBJECT)
+                },
+                required=["format"],
+            ),
+        )
+
+        req_validator = api.add_request_validator(
+            "apiReqValidator", validate_request_parameters=True, validate_request_body=True
+        )
+
+        # resp_template = """$input.path('$.body')"""
+        greetApiIntegration = apigateway.LambdaIntegration(
+            handler=runner,
+            proxy=False,
+            # passthrough_behavior=apigateway.PassthroughBehavior.NEVER,
+            # integration_responses=[
+            #     apigateway.IntegrationResponse(
+            #         status_code="200",
+            #         # selection_pattern="2\d{2}",  # Use for mapping Lambda Errors
+            #         response_parameters={
+            #             "method.response.header.Access-Control-Allow-Headers": "'cache-control,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+            #             "method.response.header.Content-Type": "'application/json'",
+            #         },
+            #         response_templates={"application/json": f"{resp_template}"},
+            #     )
+            # ],
+        )
+
+        # Because this is NOT a proxy integration, we need to define our response model
+        response_model = api.add_model(
+            "ResponseModel",
+            content_type="application/json",
+            model_name="MiztiikResponseModel",
+            schema=apigateway.JsonSchema(
+                schema=apigateway.JsonSchemaVersion.DRAFT4,
+                title="updateResponse",
+                type=apigateway.JsonSchemaType.OBJECT,
+                properties={
+                    "message": apigateway.JsonSchema(type=apigateway.JsonSchemaType.STRING)
+                },
+            ),
+        )
+
+        validate.add_method(
+            "POST",
+            integration=greetApiIntegration,
+            request_validator=req_validator,
+            request_models={"application/json": request_model},
+            method_responses=[
+                apigateway.MethodResponse(
+                    status_code="200",
+                    response_parameters={
+                        "method.response.header.Content-Type": True,
+                        "method.response.header.Access-Control-Allow-Headers": True,
+                    },
+                    response_models={"application/json": response_model},
+                ),
+                apigateway.MethodResponse(
+                    status_code="400",
+                    response_parameters={
+                        "method.response.header.Content-Length": True,
+                    },
+                    response_models={"application/json": apigateway.EmptyModel()},
+                ),
+            ],
+        )
